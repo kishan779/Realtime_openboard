7c8c0cf70deadc2e94956030d7369a42fb17c245
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2023-10-22 21:29:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const app = express(); //Initialized and server ready
 app.use(express.static("public"));
 
 let port = process.env.PORT || 5000;
-
-//let port = 5000;
-
 let server = app.listen(port, () => {
     console.log("Listening to port" + port);
 })
